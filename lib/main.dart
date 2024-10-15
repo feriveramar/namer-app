4a9da5d2e1e5c25c5a0706b03bfe698b9dd726f2
feriveramar <fernandaa.riveramartinez@gmail.com>
2024-10-15 16:43:32 -0600

    update design final master

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:rflutter_alert/rflutter_alert.dart'; 
 
 
 void main() {
